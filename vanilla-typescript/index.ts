a14ecf408ed19128467c573a50111272cca9b82b
Andrew Davis <atd285@gmail.com>
2020-05-13 21:28:31 -0400

    API calls and util functions

--- vanilla-typescript/index.ts
+++ vanilla-typescript/index.ts
@@ -2,23 +2,38 @@ const form: HTMLFormElement = document.querySelector('form');
 const input: HTMLInputElement = document.querySelector(
   'input[name="searchTerm"]'
 );
+
+const imgSection: HTMLElement = document.querySelector('.images');
 const loadingSpinner: HTMLImageElement = document.querySelector(
   '#loadingImage'
 );
 import { getImages } from './utils/API.js';
-// hide the spinner on page load
-loadingSpinner.style.display = 'none';
+document.addEventListener('DOMContentLoaded', () => {
+  loadingSpinner.style.visibility = 'hidden';
+});
 
-function handleSubmit(e: Event) {
-  loadingSpinner.style.display = '';
+async function handleSubmit(e: Event) {
   e.preventDefault();
   const formData: FormData = new FormData(form);
   const searchTerm = formData.get('searchTerm');
-  setTimeout((): (() => void) => {
-    loadingSpinner.style.display = 'none';
-    return;
-  }, 3000);
-  getImages(searchTerm);
+  getImages(searchTerm).then(paintImagesToDOM);
+}
+
+// function to map over images and toggle loading spinner off
+function paintImagesToDOM(
+  items: Array<{
+    image: string;
+    title: string;
+    author: string;
+    source: string;
+  }>
+) {
+  items.forEach(({ title }) => {
+    const p = document.createElement('p');
+    p.textContent = title;
+    imgSection.appendChild(p);
+    document.removeChild(loadingSpinner);
+  });
 }
 
 form.addEventListener('submit', handleSubmit);
--- vanilla-typescript/utils/API.ts
+++ vanilla-typescript/utils/API.ts
@@ -1,4 +1,11 @@
-export function getImages(searchTerm: FormDataEntryValue) {
-  console.log(searchTerm);
-  return searchTerm;
+const IMAGE_ENDPOINT = 'https://nature-image-api.now.sh/';
+export async function getImages(searchTerm: FormDataEntryValue) {
+  const request = await fetch(`${IMAGE_ENDPOINT}search?q=${searchTerm}`);
+  // make sure status code is okay
+  if (request.ok) {
+    const json = await request.json();
+    return json.images;
+  } else {
+    throw new Error(request.statusText);
+  }
 }
